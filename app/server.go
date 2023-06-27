cc128cf88d23ebe193e9c0d7e2d8fe557cd530ea
2814109 <2814109k@gmail.com>
2023-06-27 21:19:05 +0900

    dockerize

---
+++ app/server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	hello := []byte("Hello World!!! test")
+	_, err := w.Write(hello)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+	fmt.Println("Server Start Up........")
+	log.Fatal(http.ListenAndServe("0.0.0.0:18888", nil))
+}
